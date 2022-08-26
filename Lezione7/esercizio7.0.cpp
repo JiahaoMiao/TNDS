34a4e059d59103104a6c1c473363e961df3faf51
JiahaoMiao <94160949+JiahaoMiao@users.noreply.github.com>
2022-08-26 12:54:27 +0200

    Add files via upload

---
+++ Lezione7/esercizio7.0.cpp
@@ -0,0 +1,120 @@
+#include <cmath>
+#include <cassert>
+#include <vector>
+
+#include "gplot++.h"
+#include "fmtlib.h"
+#include "funzioneBase.h"
+#include "integrale.h"
+
+bool are_close(double calculated,double expected, double epsilon =1e-7){
+    return fabs(calculated-expected)<epsilon;
+}
+
+void test_midpoint(){
+    Seno mysin{};
+    MidPoint mp{}; 
+ 
+    assert(are_close(mp.integrate(0, M_PI, mysin, 10), 2.0082484079079745));
+    assert(are_close(mp.integrate(0, M_PI, mysin, 100), 2.000082249070986));
+    assert(are_close(mp.integrate(M_PI, 0, mysin, 10), -2.0082484079079745));
+    assert(are_close(mp.integrate(0, 1, mysin, 10), 0.45988929071851814));
+    assert(are_close(mp.integrate(1, 2, mysin, 30), 0.9564934239032155));
+}
+ 
+void test_simpson(){
+    Seno mysin{};
+    Simpson mp{};
+    //fmt::print("{}\n",mp.integrate(0, M_PI, mysin, 10));
+    assert(are_close(mp.integrate(0, M_PI, mysin, 10), 2.0001095173150043));
+    assert(are_close(mp.integrate(0, M_PI, mysin, 100), 2.000000010824504));
+    assert(are_close(mp.integrate(M_PI, 0, mysin, 100),-2.000000010824504));
+    assert(are_close(mp.integrate(0, 1, mysin, 10), 0.45969794982382056));
+    assert(are_close(mp.integrate(1, 2, mysin, 30), 0.9564491489761575));
+ 
+}
+
+void test_trapezoidi(){
+    Seno mysin{};
+    Trapezoide mp{};
+    double appo = mp.integrate(0, M_PI, mysin, 10);
+    fmt::print("{} {}\n",appo,mp.getError());
+    assert(are_close(mp.integrate(0, M_PI, mysin, 10),  1.9835235375094546));
+    assert(are_close(mp.integrate(0, M_PI, mysin, 100), 1.99983550388744364));
+    assert(are_close(mp.integrate(M_PI, 0, mysin, 100), -1.99983550388744364));
+    assert(are_close(mp.integrate(0, 1, mysin, 10), 0.45931454885797635));
+    assert(are_close(mp.integrate(1, 2, mysin, 30), 0.956360580669458));
+ 
+}
+ 
+int main(int argc,char * argv[]){
+    //if(argc!=2){
+      //  fmt::print("Usage: ./esercuzio7.0 nstep\n");
+      //  return 1;
+    //}
+    //test_midpoint();
+    //test_simpson();
+    //test_trapezoidi();
+
+    
+    double a{};
+    double b{M_PI};
+
+    //int nstep{atoi(argv[1])};  
+
+    Seno mysin{};
+    MidPoint midpoint{};
+    Simpson simpson{}; 
+    Trapezoide trapezoide{};
+    //Integral* integrator{}; 
+ 
+    //fmt::print("midpoint: {:.12f}\n",midpoint.integrate(b,a,nstep,mysin));
+    //fmt::print("Simpson: {:.12f}\n",integrator->integrate(a,b,nstep,mysin));
+
+
+
+    std::vector<double> steps{10, 50, 100, 500, 1000};
+    std::vector<double> errors(steps.size());
+
+    for (size_t i{}; i < errors.size(); ++i) {
+        //errors[i] = fabs(midpoint.integrate(a,b,steps[i],mysin)-2); // Riempire il valore corrispondente
+        errors[i] = 4*fabs(midpoint.integrate(a,b,mysin,steps[i])-midpoint.integrate(a,b,mysin,2*steps[i]))/3;
+    }
+
+    double appo = (log(errors[errors.size()-1])-log(errors[0]))/(log(steps[steps.size()-1])-log(steps[0]));
+    fmt::print("Midpoint = {}\n",appo);
+
+    Gnuplot plot{};
+    const std::string output_file_name{"error.png"};
+    plot.redirect_to_png(output_file_name, "800,600");
+    plot.set_xlabel("Numero di passi");
+    plot.set_ylabel("Errore");
+    plot.set_logscale(Gnuplot::AxisScale::LOGXY);
+    plot.plot(steps, errors,"midpoint",Gnuplot::LineStyle::LINESPOINTS);
+    
+    // È sempre bene avvisare l'utente che è stato creato un file e fornirgli il
+    // nome.
+    std::cout << "Plot salvato nel file " << output_file_name << std::endl;
+
+    for (size_t i{}; i < errors.size(); ++i) {
+        //errors[i] = fabs(midpoint.integrate(a,b,steps[i],mysin)-2); // Riempire il valore corrispondente
+        errors[i] = 16*fabs(simpson.integrate(a,b,mysin,steps[i])-simpson.integrate(a,b,mysin,2*steps[i]))/15;
+    }
+
+    appo = (log(errors[errors.size()-1])-log(errors[0]))/(log(steps[steps.size()-1])-log(steps[0]));
+    fmt::print("Simpson = {}\n",appo);
+
+    plot.plot(steps, errors,"Simpson",Gnuplot::LineStyle::LINESPOINTS);
+
+    for (size_t i{}; i < errors.size(); ++i) {
+        //errors[i] = fabs(midpoint.integrate(a,b,steps[i],mysin)-2); // Riempire il valore corrispondente
+        errors[i] = 4*fabs(trapezoide.integrate(a,b,mysin,steps[i])-trapezoide.integrate(a,b,mysin,2*steps[i]))/3;
+    }
+    appo = (log(errors[errors.size()-1])-log(errors[0]))/(log(steps[steps.size()-1])-log(steps[0]));
+    fmt::print("Trapezoide = {}\n",appo);
+
+    plot.plot(steps, errors,"Trapezoide",Gnuplot::LineStyle::LINESPOINTS);
+    plot.show();
+    
+    return 0;
+} 
---
+++ Lezione7/integrale.h
@@ -0,0 +1,260 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include "funzioneBase.h"
+
+class Integral{
+    public:
+    Integral():m_a{},m_b{},m_sign{},m_step(),m_prec{},m_err{}{}
+
+    double integrate(double a,double b,const FunzioneBase &f,unsigned int nstep=1e3,double precision=1e-6,bool fixed_precision=false){
+        checkInterval(a,b);
+        setPrecision(precision);
+        setPartition(nstep);
+        setError(0);
+        if(fixed_precision == true){
+            return calculate(f);
+        }else{
+            return calculate(nstep,f);
+        }
+    }
+
+    double integrate(double a,double b,std::function<double(double)> f,unsigned int nstep=1e3,double precision=1e-6,bool fixed_precision=false){
+        checkInterval(a,b);
+        setPrecision(precision);
+        setPartition(nstep);
+        setError(0);
+        if(fixed_precision == true){
+            return calculate(f);
+        }else{
+            return calculate(nstep,f);
+        }
+    }
+    
+
+    double getA() const {return m_a;}
+    double getB() const {return m_b;}
+    int getSign() const {return m_sign;}
+    unsigned int getPartition() const {return m_step;}
+    double getError() const {return m_err;}
+    double getPrecision() const {return m_prec;}
+
+    void setA(double a) {m_a=a;}
+    void setB(double b)  {m_b=b;}
+    void setSign(int s) {m_sign=s;}
+    void setPartition(unsigned int n) {m_step=n;}
+    void setError(double err) {m_err=err;}
+    void setPrecision(double p) {m_prec=p;}
+
+    
+    private:
+
+    virtual double calculate(unsigned int nstep,const FunzioneBase &f)const =0;
+    virtual double calculate(unsigned int nstep,std::function<double(double)> f)const=0;
+
+    virtual double calculate(const FunzioneBase &f) =0;
+    virtual double calculate(std::function<double(double)> f)=0;
+    void checkInterval(double a,double b){
+        m_a=std::min(a,b);
+        m_b=std::max(a,b);
+        m_sign= (a<b) ? 1 : -1;
+    }
+
+    double m_a,m_b;
+    int m_sign;
+    unsigned int m_step;
+    double m_prec;
+    double m_err;
+};
+
+class MidPoint:public Integral{
+    public:
+    MidPoint():Integral(){}
+
+    private:
+
+    // ================================================
+    // Fixed Partition
+    // ================================================
+
+    double calculate(unsigned int nstep,const FunzioneBase &f)  const override{
+        double h{(getB()-getA())/nstep};
+        double sum{};
+
+        for(unsigned int i{};i<nstep;i++){
+            sum+= f.Eval(getA()+(i+0.5)*h);
+        }
+
+        return getSign()*sum*h;
+    }
+
+    double calculate(unsigned int nstep,std::function<double(double)>f) const override{
+        double h{(getB()-getA())/nstep};
+        double sum{};
+
+        for(unsigned int i{};i<nstep;i++){
+            sum+= f(getA()+(i+0.5)*h);
+        }
+
+        return getSign()*sum*h;
+    }
+    // ================================================
+    // Fixed Precision 
+    // ================================================
+    virtual double calculate(const FunzioneBase &f)override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(4*fabs(sum-old_sum)/3);
+        }while(getError()>getPrecision());
+        //return sum;
+        return (4*sum-old_sum)/3;
+    }
+    virtual double calculate(std::function<double(double)> f)override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+        
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(4*fabs(sum-old_sum)/3);
+        }while(getError()>getPrecision());
+
+        return (4*sum-old_sum)/3;
+    }
+};
+
+
+class Simpson:public Integral{
+    public:
+    Simpson():Integral(){}
+
+    private:
+    // ================================================
+    // Fixed Partition 
+    // ================================================
+
+    double calculate(unsigned int nstep,const FunzioneBase &f) const override{
+        unsigned int truen = (nstep %2==0) ? nstep:(nstep-1);
+        double h{(getB()-getA())/truen};
+        double sum{(f.Eval(getA())+ f.Eval(getB()))/3};
+
+        for(unsigned int i{1};i<truen;i++){
+            sum+= 2*(1+i%2)*f.Eval(getA()+i*h)/3;
+        }
+        return getSign()*sum*h;
+    }
+
+    double calculate(unsigned int nstep,std::function<double(double)>f) const override{
+        unsigned int truen = (nstep %2 ==0) ? nstep:(nstep-1);
+        double h{(getB()-getA())/truen};
+        double sum{(f(getA())+ f(getB()))/3};
+
+        for(unsigned int i{1};i<truen;i++){
+            sum+= 2*(1+i%2)*f(getA()+i*h)/3;
+        }
+        return getSign()*sum*h;
+    }
+
+    // ================================================
+    // Fixed Precision 
+    // ================================================
+    virtual double calculate(const FunzioneBase &f)override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(16*fabs(sum-old_sum)/15);
+        }while(getError()>getPrecision());
+        //return sum;
+        return (16*sum-old_sum)/15;
+        //return (4*sum-old_sum)/3;
+    }
+    virtual double calculate(std::function<double(double)> f)override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(16*fabs(sum-old_sum)/15);
+        }while(getError()>getPrecision());
+
+        return (16*sum-old_sum)/15;
+    }
+};
+
+class Trapezoide:public Integral{
+    public:
+    Trapezoide():Integral(){}
+
+    private:
+    // ================================================
+    // Fixed Partition 
+    // ================================================
+
+    double calculate(unsigned int nstep,const FunzioneBase &f)const override{
+        //Parametri iniziali
+        nstep = (nstep%2==0) ? nstep : (nstep-1);
+        double sum{(f.Eval(getA())+f.Eval(getB()))/2};
+        double h{(getB()-getA())/nstep};  //Passo h
+        for(unsigned int i{1};i<nstep;i++){
+            sum+= f.Eval(getA()+i*h);
+        }
+        return getSign()*sum*h;
+    }
+
+
+    double calculate(unsigned int nstep,std::function<double(double)>f)const override{
+
+        nstep = (nstep%2==0) ? nstep : (nstep-1);
+        double sum{(f(getA())+f(getB()))/2};
+        double h{(getB()-getA())/nstep};  //Passo h
+        for(unsigned int i{1};i<nstep;i++){
+            sum+= f(getA()+i*h);
+        }
+        return getSign()*sum*h;
+    } 
+
+    // ================================================
+    // Fixed Precision 
+    // ================================================
+    virtual double calculate(const FunzioneBase &f) override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(4*fabs(sum-old_sum)/3);
+        }while(getError()>getPrecision());
+        //return sum;
+        return (4*sum-old_sum)/3;
+    }
+    virtual double calculate(std::function<double(double)> f) override{
+        double old_sum{};
+        double sum{calculate(getPartition(),f)};
+
+        do{
+            old_sum=sum;
+            setPartition(2*getPartition());
+            sum = calculate(getPartition(),f);
+            setError(4*fabs(sum-old_sum)/3);
+        }while(getError()>getPrecision());
+
+        return (4*sum-old_sum)/3;
+    }
+};
